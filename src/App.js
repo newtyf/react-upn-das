dfe831081a40f2b49d61b226edbf3aacf8b48b05
Axel <adolfo.luna@upn.pe>
2025-04-30 20:56:21 -0500

    feat: se agregaro los cambios y se ajustaron estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const formReducer = (state, event) => {
+ return {
+   ...state,
+   [event.name]: event.value
+ }
 }
+
+function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [formData2, setFormData2] = useReducer(formReducer, {});
+  const [formData3, setFormData3] = useReducer(formReducer, {});
+  const [formData4, setFormData4] = useReducer(formReducer, {});
+  const [formData5, setFormData5] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 7000);
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange2 = event => {
+    setFormData2({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange3 = event => {
+    setFormData3({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange4 = event => {
+    setFormData4({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange5 = event => {
+    setFormData5({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+
+  return(
+    <div className="estilo">
+      <h1>Usando formularios y eventos en React</h1>
+      {submitting ?
+       <div>
+         Tu realizaste Submit para la siguiente información:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>Nombres</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(formData2).map(([name, value]) => (
+             <li key={name}><strong>Apellidos</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(formData3).map(([name, value]) => (
+             <li key={name}><strong>Fecha de nacimiento</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(formData4).map(([name, value]) => (
+             <li key={name}><strong>DIreccion</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(formData5).map(([name, value]) => (
+             <li key={name}><strong>Telefomo</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+       </div>
+       :
+       <form onSubmit={handleSubmit}>
+       <fieldset>
+         <label>
+           <p>Nombres</p>
+           <input name="name" onChange={handleChange}/>
+         </label>
+         <label>
+           <p>Apellidos</p>
+           <input name="apellidos" onChange={handleChange2}/>
+         </label>
+         <label>
+           <p>Fecha de nacimiento</p>
+           <input name="date" onChange={handleChange3}/>
+         </label>
+         <label>
+           <p>DIreccion</p>
+           <input name="direction" onChange={handleChange4}/>
+         </label>
+         <label>
+           <p>Telefomo</p>
+           <input name="tel" onChange={handleChange5}/>
+         </label>
+       </fieldset>
+       <button type="submit">Submit</button>
+     </form>
+      }
+
+    </div>
+  )
+}
+
+
 
 export default App;
